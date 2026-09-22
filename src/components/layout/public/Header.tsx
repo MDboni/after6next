d9939b5b7c0c5bbb9b638c6adf6428d2e050555a
Boni Amin Jayed <aminboni048@gmail.com>
2026-09-22 21:02:22 +0600

    Implement authentication and user management features with hooks and API integration

---
+++ src/components/layout/public/Header.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import Logo from "@/assets/svg/Logo";
+import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/toast";
+import { useGetMe, useLogout } from "@/hooks";
+import { UserRole } from "@/types";
+import { useQueryClient } from "@tanstack/react-query";
+import Link from "next/link";
+
+export default function Header() {
+  const routes = [
+    { name: "Home", url: "/" },
+    { name: "About us", url: "/about-us" },
+  ];
+
+  const dashboardRoute: Record<UserRole, string> = {
+    SUPER_ADMIN: "/admin",
+    ADMIN: "/admin",
+    DOCTOR: "/doctor",
+    PATIENT: "/patient",
+  };
+
+  const { data, isLoading } = useGetMe();
+  const { mutate: logout } = useLogout();
+  const queryClient = useQueryClient();
+
+  const role: UserRole = !!data?.data && data?.data.role;
+
+  const handleLogout = () => {
+    logout(undefined, {
+      onSuccess: () => {
+        toast.add({
+          title: "Tata",
+          description: "Logged out successfully",
+          type: "success",
+        });
+        queryClient.removeQueries({ queryKey: ["user"] });
+      },
+      onError: () => {
+        toast.add({
+          title: "Logout failed",
+          description: "Something Went Wrong",
+          type: "error",
+        });
+      },
+    });
+  };
+
+  return (
+    <header className="w-full h-16 border border-b">
+      <div className="flex justify-between items-center h-full max-w-7xl mx-auto">
+        <div className="flex items-center gap-2">
+          <Logo />
+          <span>PH Healthcare</span>
+        </div>
+
+        <nav className="flex gap-5">
+          {routes.map((route) => (
+            <Link key={route.url} href={route.url}>
+              {route.name}
+            </Link>
+          ))}
+
+          {role && <Link href={dashboardRoute[role]}>Dashboard</Link>}
+        </nav>
+        <div>
+          {!isLoading && !data && (
+            <Button
+              variant="outline"
+              render={<Link href="/login">Login</Link>}
+              nativeButton={false}
+            >
+              Login
+            </Button>
+          )}
+          {!isLoading && data && (
+            <Button onClick={handleLogout} variant="destructive">
+              Logout
+            </Button>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
