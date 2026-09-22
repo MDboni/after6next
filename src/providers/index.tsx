01f54ba8f63a8d07c0389b62d59777a03e83a827
Boni Amin Jayed <aminboni048@gmail.com>
2026-09-22 21:23:24 +0600

    Add Google OAuth integration, implement toast and tooltip components, and update footer with copyright

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { ReactNode } from "react";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import GoogleAuthProvider from "./google-auth.provider";
 import QueryProvider from "./query.provider";
-
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-  
+    <GoogleAuthProvider>
       <QueryProvider>
         <TooltipProvider>{children}</TooltipProvider>
       </QueryProvider>
- 
+    </GoogleAuthProvider>
   );
 }
